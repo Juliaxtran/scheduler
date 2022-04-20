ce4946fd52d075ec0f6568517b5a6742f9bb3773
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-04-20 17:21:48 -0400

    Add Confirm Component with Stories

--- stories/index.js
+++ stories/index.js
@@ -7,9 +7,11 @@ import DayListItem from "components/DayListItem"
 import DayList from "components/DayList"
 import InterviewerListItem from "components/InterviewerListItem"
 import InterviewerList from "components/InterviewerList"
-import Appointment from "components/Appointment/index.js";
-import Header from "components/Appointment/Header.js";
-import Empty from "components/Appointment/Empty.js";
+import Appointment from "components/Appointment/index";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 
 storiesOf("Button", module)
@@ -138,4 +140,14 @@ storiesOf("InterviewerList", module)
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
-  .add("Empty", () => <Empty  onAdd={action("onAdd")}/>);
+  .add("Empty", () => <Empty  onAdd={action("onAdd")}/>)
+  .add("Show", () => <Show student="Lydia Miller-Jones"
+  interviewer={interviewer}
+  onEdit={action("onEdit")}
+  onDelete={action("onDelete")}
+  />)
+  .add("Confirm", () => <Confirm
+  message="Delete the appointment?"
+  onConfirm={action("onConfirm")}
+  onCancel={action("onCancel")}
+  />);
