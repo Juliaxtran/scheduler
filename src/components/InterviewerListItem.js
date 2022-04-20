1113d508ddcbf0bf60b207ff210898d8c3a72c29
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-04-20 10:10:12 -0400

    Add props to Interviewer List Items

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import "./InterviewerListItem.scss";
+import classNames from "classnames";
 
-function InterviewerListItem(){
+
+
+
+
+
+function InterviewerListItem(props){
+
+  const listClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected
+  })
+
 return (
-<li className="interviewers__item">
+<li onClick={()=> props.setInterviewer(props.id)} key={props.id} className={listClass}>
   <img
     className="interviewers__item-image"
-    src="https://i.imgur.com/LpaY82x.png"
-    alt="Sylvia Palmer"
+    src={props.avatar}
+    alt={props.name}
   />
-  Sylvia Palmer
+ {props.name}
 </li>
 );
 }
