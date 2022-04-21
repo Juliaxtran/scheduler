8afc525c0ccec452a7824af6e4c515e36646c9c7
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-04-20 20:47:42 -0400

    Adding Appointment Data and Appoitnment to App

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { useState } from "react";
-
 import "components/Application.scss";
-import DayList from 'components/DayList';
-
-
+import DayList from "./DayList";
+import { useState } from "react";
+import "components/Appointment";
+import Confirm from "./Appointment/Confirm";
+import Empty from "./Appointment/Empty";
+import Appointment from "components/Appointment";
 const days = [
   {
     id: 1,
@@ -22,10 +23,55 @@ const days = [
     spots: 0,
   },
 ];
-
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
-
+  const appointmentList = Object.values(appointments).map((appointment) => {
+    return (
+      <Appointment
+        key={appointment.id}
+        {...appointment}
+      />
+    )
+  }
+  );
   return (
     <main className="layout">
       <section className="sidebar">
@@ -36,7 +82,6 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-
           <DayList
             days={days}
             value={day}
@@ -50,9 +95,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentList}
       </section>
-
     </main>
   );
 }
