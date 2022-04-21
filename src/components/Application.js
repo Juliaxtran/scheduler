6cdb56a2862b6fff75369da9c74397d58bea747a
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-04-21 19:37:03 -0400

    Change useState functions in Application

--- src/components/Application.js
+++ src/components/Application.js
@@ -47,15 +47,30 @@ const appointments = {
     time: "4pm",
   }
 };
+
+
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
+
+  // const setDays = days => setState({...state, days})
+
+
+
   //get request /api/days
   useEffect(() => {
     axios.get(`/api/days`).then(response => {
-      setDays([...response.data]);
+      setDays(response.data);
     });
   }, []);
+
+
   const appointmentList = Object.values(appointments).map((appointment) => {
     return (
       <Appointment
@@ -65,6 +80,9 @@ export default function Application(props) {
     )
   }
   );
+
+
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -76,8 +94,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
---
+++ src/helpers/selectors.js
@@ -0,0 +1,17 @@
+export function getAppointmentsForDay(state, day) {
+
+  let appointmentIdArray = [];
+
+  for (const currentDay of state.days) {
+    if (currentDay.name === day) {
+      appointmentIdArray = currentDay.appointments;
+    }
+  }
+
+  const result = appointmentIdArray.map(appointmentId => {
+    return state.appointments[appointmentId];
+  });
+
+  return result;
+
+}
