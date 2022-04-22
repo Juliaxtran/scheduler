3adeb949e6272a5a0f404e1e0a3eefb9a22bb131
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-04-22 18:20:29 -0400

    Use Custom Hooks in Application

--- src/components/Application.js
+++ src/components/Application.js
@@ -9,6 +9,11 @@ import Appointment from "components/Appointment";
 import axios from 'axios';
 import { useEffect } from "react";
 import { getAppointmentsForDay, getInterview } from "../helpers/selectors"
+
+
+
+
+
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
@@ -26,7 +31,7 @@ export default function Application(props) {
       setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }))
     });
   }, []);
-  
+
   const dailyAppointments = getAppointmentsForDay(state, state.day);
 
   const appointmentList = dailyAppointments.map((appointment) => {
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,22 +1,42 @@
+import React from 'react';
+import "../Appointment/styles.scss"
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+import { Fragment } from "react";
+import useVisualMode from "hooks/useVisualMode";
+import Form from './Form';
 
-import React, { Fragment } from 'react';
-import "components/Appointment/styles.scss";
-import Header from "components/Appointment/Header"
-import Show from "components/Appointment/Show"
-import Empty from "components/Appointment/Empty"
 
 function Appointment(props) {
 
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
-  <Fragment>
-    <Header time={props.time}/>
-    <article className="appointment">
-    {props.interview ?
-     <Show
-     student={props.interview.student}
-     interviewer={props.interview.interviewer}/> : <Empty/> }
-    </article>
-   </Fragment>
+    <Fragment>
+      <Header time={props.time} />
+      <article className="appointment">
+        {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+        {mode === SHOW && (
+          <Show
+            student={props.interview.student}
+            interviewer={props.interview.interviewer}
+          />
+        )}
+         {mode === CREATE &&
+         <Form
+         interviewers={[]}
+         onCancel={() => back(EMPTY)}
+         />}
+
+        </article>
+    </Fragment>
 
   );
 }
