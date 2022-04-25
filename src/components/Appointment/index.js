96f7f780bb44e2509c5b0b75c860d991260411a4
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-04-25 15:57:56 -0400

    Add saving transition to index

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,36 +3,43 @@ import "../Appointment/styles.scss"
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Status from './Status';
 import { Fragment } from "react";
+
 import useVisualMode from "hooks/useVisualMode";
 import Form from './Form';
 function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const SAVING = "SAVING";
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
 
   function save(name, interviewer) {
-    
-    const interview = {
-      student: name,
-      interviewer
-    };
 
+    if (name && interviewer) {
+      transition(SAVING);
 
+      const interview = {
+        student: name,
+        interviewer
+      };
 
-    props.bookInterview(props.id, interview).then(() => transition(SHOW));
+      props.bookInterview(props.id, interview).then(() => transition(SHOW));
+    }
 
   }
+
 
   return (
     <Fragment>
       <Header time={props.time} />
       <article className="appointment">
         {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
-        {mode === SHOW  && (
+        {mode === SAVING && <Status message="Saving" />}
+        {mode === SHOW && (
           <Show
             student={props.interview.student}
             interviewer={props.interview.interviewer}
