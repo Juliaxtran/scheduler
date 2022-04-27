41a9297a9592c30db8d4560a6e35b62476a17e3c
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-04-27 16:56:12 -0400

    Book Interview Test

---
+++ cypress/integration/appointments.spec.js
@@ -0,0 +1,23 @@
+describe("Appoitment", () => {
+  beforeEach(() => {
+    cy.request("GET", "/api/debug/reset");
+    cy.visit("/");
+    cy.contains("Tuesday")
+      .click({ multiple: true })
+  })
+
+  it("should book an interview", () => {
+    cy.get("[alt=Add]")
+      .first()
+      .click();
+
+    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
+    cy.get('[alt="Sylvia Palmer"]').click();
+
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
+
+});
